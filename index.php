c201455098081f000f623c0e5bc56265c0f58660
carlosbarrancom <carlosmanuel.barranco.martin.alu@iesjulioverne.es>
2020-12-15 09:16:26 +0100

    Añado a la tabla del 2

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 	for($i = 1;$i<=10;$i++) {
-		echo "2 x $i = " .2*$i ."<br>";
+		echo "2 x $i = ". 2*$i ."<br>";
+	}
+
+	echo "<br><br>";
+
+	for($i = 1;$i<=10;$i++) {
+		echo "3 x $i = ". 3*$i ."<br>";
 	}
 ?>
